c9ad9ca5f38d2166ba70edd2a02bc72da29f82c4
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-10 20:57:39 +0000

    add stomp endpoints configuration

---
+++ demo/src/main/java/com/example/demo/config/webSocketConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;;
+
+@EnableWebSocketMessageBroker
+public class webSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp-endpoint")
+            .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+}
