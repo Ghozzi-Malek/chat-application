e450893a6a558329ab6b732b7566e787351cf968
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-11 23:49:20 +0000

    simple entity for testing message routing

---
+++ demo/src/main/java/com/example/demo/entities/MessageTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageTest {
+    private String text;
+}
