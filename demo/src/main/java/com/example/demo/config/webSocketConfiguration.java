0960ac9e73c3757718f0d71a0ad59794b3a9b3c2
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-11 23:08:39 +0000

    fix config file

--- demo/src/main/java/com/example/demo/config/webSocketConfiguration.java
+++ demo/src/main/java/com/example/demo/config/webSocketConfiguration.java
@@ -1,11 +1,12 @@
 package com.example.demo.config;
 
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-// import org.springframework.web.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;;
 
+@Configuration
 @EnableWebSocketMessageBroker
 public class webSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
