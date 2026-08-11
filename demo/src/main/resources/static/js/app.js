e2b1df3d2a9fcb821a386f7c22805a5572973ff6
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-11 23:44:36 +0000

    connect client to server using websockets

---
+++ demo/src/main/resources/static/js/app.js
@@ -0,0 +1,65 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/stomp-endpoint');
+    stompClient = Stomp.over(socket);
+    // enable client debug logs in the browser console
+    stompClient.debug = console.log;
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        // subscribe to the server destination that the backend sends to
+        stompClient.subscribe('/topic/messages', function (greeting) {
+            showGreeting(JSON.parse(greeting.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    if (!stompClient) return;
+    var text = $("#message").val();
+    if (!text || text.trim() === '') return;
+    stompClient.send("/app/chat.send", {}, JSON.stringify({'text': text}));
+    // clear input after sending
+    $("#message").val('');
+}
+
+function showGreeting(message) {
+    // append a message element into the message feed
+    $("#greetings").append(
+        "<article class='message outgoing'><div class='message-avatar'>Y</div>" +
+        "<div class='message-body'><div class='message-meta'><strong>You</strong><span></span></div>" +
+        "<p>" + message.text + "</p></div></article>"
+    );
+}
+
+$(function(){
+    connect();
+})
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#send" ).click(function() { sendName(); });
+});
