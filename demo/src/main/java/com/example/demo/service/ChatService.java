f97422aa174b40f95cf8fe167a2413ca29af4a2b
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-14 20:51:27 +0000

    feautre: get a user chat

--- demo/src/main/java/com/example/demo/service/ChatService.java
+++ demo/src/main/java/com/example/demo/service/ChatService.java
@@ -2,21 +2,24 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Chat;
 import com.example.demo.repository.ChatsRepository;
+import com.example.demo.repository.ChatsRepositoryImp;
 
 @Service
 public class ChatService {
-    private final ChatsRepository chatsRepository;
+    private final ChatsRepositoryImp chatsRepository;
 
-    public ChatService(ChatsRepository chatsRepository){
+    public ChatService(ChatsRepositoryImp chatsRepository){
         this.chatsRepository = chatsRepository;
     }
 
-    public List<Chat> getChats(){
-        
+    public List<String> getChats(Authentication auth){
+        String userId = auth.getName();
+        return chatsRepository.getChats(userId);
     }
 }
