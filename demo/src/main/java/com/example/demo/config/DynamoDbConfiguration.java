bd9f184e7cd4e09bce8cc269ce22a054a2a92806
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-08-19 21:56:08 +0000

    add all tables to config

--- demo/src/main/java/com/example/demo/config/DynamoDbConfiguration.java
+++ demo/src/main/java/com/example/demo/config/DynamoDbConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.entities.Chat;
+import com.example.demo.entities.ChatMessage;
+import com.example.demo.entities.Members;
 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
@@ -45,4 +47,17 @@ public class DynamoDbConfiguration {
             @Value("${aws.dynamodb.chat-table:Chat}") String tableName) {
         return dynamoDbEnhancedClient.table(tableName, TableSchema.fromBean(Chat.class));
     }
+    @Bean
+    DynamoDbTable<ChatMessage> MessageTable(
+            DynamoDbEnhancedClient dynamoDbEnhancedClient,
+            @Value("${aws.dynamodb.message-table}") String tableName) {
+        return dynamoDbEnhancedClient.table(tableName, TableSchema.fromBean(ChatMessage.class));
+        
+    }
+    @Bean
+    DynamoDbTable<Members> membersTable(
+        DynamoDbEnhancedClient client,
+        @Value("${aws.dynamodb.members-table}") String tableName) {
+    return client.table(tableName, TableSchema.fromBean(Members.class));
+}
 }
