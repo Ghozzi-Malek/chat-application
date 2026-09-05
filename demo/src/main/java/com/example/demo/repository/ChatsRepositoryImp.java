492f80ac31429849e7e2831a12787a83667a014c
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-09-05 22:00:49 +0000

    feature: save messages to database

--- demo/src/main/java/com/example/demo/repository/ChatsRepositoryImp.java
+++ demo/src/main/java/com/example/demo/repository/ChatsRepositoryImp.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import com.example.demo.entities.Chat;
+import com.example.demo.entities.ChatMessage;
 import com.example.demo.entities.Members;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 @Repository
 @RequiredArgsConstructor
@@ -16,6 +18,8 @@ import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 public class ChatsRepositoryImp {
     private final DynamoDbTable<Chat> chatTable;
     private final DynamoDbTable<Members> membersTable;
+    private final DynamoDbTable<ChatMessage> messagTable;
+    
 
     public List<Chat> getChats(String userId) {
         Expression userFilter = Expression.builder()
@@ -37,6 +41,10 @@ public class ChatsRepositoryImp {
                         .build()))
                 .filter(chat -> chat != null)
                 .toList();
+    }
+
+    public void saveMessageToDb(ChatMessage message){
+        messagTable.putItem(message);
     }
 
 }
