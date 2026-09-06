27c2c3e93cd8014238c4b93472b07a846e99529b
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-09-06 23:16:09 +0000

    send messages to redis

--- demo/src/main/java/com/example/demo/controllers/testController.java
+++ demo/src/main/java/com/example/demo/controllers/testController.java
@@ -4,7 +4,13 @@ import org.apache.logging.log4j.message.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import com.example.demo.repository.ChatsRepositoryImp;
+import com.example.demo.service.ChatService;
+import com.example.demo.service.RedisMessagingService;
 
+import lombok.RequiredArgsConstructor;
+
+import com.example.demo.entities.ChatMessage;
 import com.example.demo.entities.MessageTest;
 
 
@@ -14,13 +20,17 @@ import com.example.demo.entities.MessageTest;
 
 
 @Controller
+@RequiredArgsConstructor 
 public class testController {
 
-
+    private final ChatService chatService;
+    private final RedisMessagingService messagingService;
     @MessageMapping("/chat.send")
     @SendTo("/topic/messages")
-    public MessageTest deliverMessage(MessageTest message) {
-        return new MessageTest(message.getText());
 
+    public void sendMessage(ChatMessage message){
+        chatService.deliverMessage(message);
+        messagingService.sendMessage(message.getText());
     }
+    
 }
