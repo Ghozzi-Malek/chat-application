646db381cebd35bd53f92cbce7f6ef19e9c80504
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-09-10 21:33:35 +0000

    change topic name and fix listner issue

--- demo/src/main/java/com/example/demo/config/RedisConfig.java
+++ demo/src/main/java/com/example/demo/config/RedisConfig.java
@@ -6,29 +6,22 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
 import com.example.demo.service.RedisReceiver;
+
 @Configuration 
 public class RedisConfig {
-
     @Bean 
-    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,MessageListenerAdapter listenerAdapter){
+    public RedisMessageListenerContainer container(
+            RedisConnectionFactory connectionFactory, RedisReceiver receiver) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, new PatternTopic(redisChannel()));
+        container.addMessageListener(receiver, new PatternTopic("chat:user:*") );
         return container;
     }
 
     @Bean 
-    public MessageListenerAdapter listenerAdapter(RedisReceiver receiver){
-        return new MessageListenerAdapter(receiver,"receiveMessage");
-    }
-    @Bean 
     public StringRedisTemplate template(RedisConnectionFactory connectionFactory){
         return new StringRedisTemplate(connectionFactory);
-    }
-    @Bean 
-    public String redisChannel(){
-        return "exampleapp";
     }
 }
